75fc4ad732d8485984b40b24fab83a134d0322f8
mfiaz05 <167330025+mfiaz05@users.noreply.github.com>
2024-12-06 03:16:02 +0500

    update remix

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import remix from "@remix-run/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-})
+  plugins: [remix()],
+});
